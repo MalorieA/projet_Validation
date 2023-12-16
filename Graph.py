93894806d988a4e7f3bd7bee7551cee160904876
Malorie <malorie.abou@ensta-bretagne.org>
2023-12-16 16:03:39 +0100

    Graph_roots

--- Graph.py
+++ Graph.py
@@ -3,3 +3,5 @@ class Graph:
         self.graph=dict()   #Dictionnaire pour les relations entre les noeuds
         self.roots=[]       #Liste des noeuds racines
 
+    def roots(self):
+        return self.roots   #Méthode pour retourner une liste des racines noeuds du graph
