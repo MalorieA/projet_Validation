d449d23fcba3caa2d5d513d6119a5756a9107321
Malorie <malorie.abou@ensta-bretagne.org>
2023-12-16 17:19:59 +0100

    DictRootedGraph_neighbors

--- DictRootedGraph.py
+++ DictRootedGraph.py
@@ -4,4 +4,7 @@ class DictRootedGraph(RootedGraph): # DictRootedGraph est un Class dérivée qui
         super().__init__()  # Appeler le constructeur de la classe de base en utilisant super
 
     def roots(self):
-        super().roots()     # Appeller la même méthode de la classe de base
+        super().roots()     # Appeller la même méthode roots de la classe de base
+
+    def neighbors(self,node):
+        super().neighbors()     # Appeller la même méthode neighbors de la classe de base
