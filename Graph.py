a6e64433ec8d88fe5a07b740fb932d6857961554
Malorie <malorie.abou@ensta-bretagne.org>
2023-12-16 16:49:37 +0100

    RootedGraph

--- Graph.py
+++ Graph.py
@@ -1,4 +1,4 @@
-class Graph:
+class RootedGraph:
     def __init__(self):
         self.graph=dict()   #Dictionnaire pour les relations entre les noeuds
         self.roots=[]       #Liste des noeuds racines
