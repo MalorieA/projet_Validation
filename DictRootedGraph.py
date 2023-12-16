a191c29d66563faaff6cbe16e9b234ac16b6a6aa
Malorie <malorie.abou@ensta-bretagne.org>
2023-12-16 19:22:02 +0100

    DictRootedGraph_hash

--- DictRootedGraph.py
+++ DictRootedGraph.py
@@ -1,4 +1,5 @@
 from RootedGraph import RootedGraph
+from collections import deque
 class DictRootedGraph(RootedGraph): # DictRootedGraph est un Class dérivée qui hérité de RootedGraph
     def __init(self):
         super().__init__()  # Appeler le constructeur de la classe de base en utilisant super
