72e87b78b9f65467d5934456ebed803b9416fa05
Malorie <malorie.abou@ensta-bretagne.org>
2023-12-16 17:13:42 +0100

    DictRootedGraph_init

---
+++ DictRootedGraph.py
@@ -0,0 +1,4 @@
+from RootedGraph import RootedGraph
+class DictRootedGraph(RootedGraph): # DictRootedGraph est un Class dérivée qui hérité de RootedGraph
+    def __init(self):
+        super().__init__()  # Appeler le constructeur de la classe de base en utilisant super
