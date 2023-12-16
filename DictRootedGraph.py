af64941c820456b2197394b803c0877b1fb60500
Malorie <malorie.abou@ensta-bretagne.org>
2023-12-16 18:32:12 +0100

    DictRootedGraph_equiv

--- DictRootedGraph.py
+++ DictRootedGraph.py
@@ -12,5 +12,8 @@ class DictRootedGraph(RootedGraph): # DictRootedGraph est un Class dérivée qui
     def add_new_one(self,u,v):
         self.graph.setdefault(u,[]).append(v)     # Ajoute une arête entre u et v dans le graphe
                                                   # Vérifie si u n'existe pas on l'initialise avec une liste vide avant d'ajouter v
-
-
+    def __eq__(self, other):
+        is_same_type = type(other) is DictRootedGraph       # Vérifie si other est une instance du DictRootedGraph
+        same_graph = self.graph == other.graph
+        same_roots = self.roots == other.roots
+        return is_same_type and same_graph and same_roots   # Renvoie True si other est de même type, et les attributs graph et roots sont égaux
